445e404d54999ad09ce409b139961d264e86d2ab
Mert Kaya <47145541+mertkaya13@users.noreply.github.com>
2020-09-21 17:35:26 +0300

    Add files via upload

--- simple punching game/app_punching.js
+++ simple punching game/app_punching.js
@@ -2,13 +2,26 @@ new Vue({
     el:'#vue-app',
     data: {
         health: 100,
-        ended: false
+        ended: false,
+        x : 0,
+        y : 0
     },
     methods: {
+        updatexy: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
         punch: function(){
-            this.health -= 10;
-            if( this.health <= 0){
-                this.ended = true;
+            if(this.x>41 && this.x<160 && this.y>190 && this.y<439){
+                if(this.y > 420){
+                    if(this.x < 43 || this.x >154){
+                        return;
+                    }
+                }
+                this.health -= 10;
+                if( this.health <= 0){
+                    this.ended = true;
+                }
             }
         },
         restart: function(){
